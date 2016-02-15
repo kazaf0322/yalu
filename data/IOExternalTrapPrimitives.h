72d4665514b0c22cae30829ea9eab8944a3f8013
kazaf0322 <yeelamz@gmail.com>
2016-02-15 22:23:03 +0800

    Create IOExternalTrapPrimitives.h

---
+++ data/IOExternalTrapPrimitives.h
@@ -0,0 +1,17 @@
+#ifndef YaluKernel_h
+#define YaluKernel_h
+ 
+struct YaluKernel {
+    vm_address_t kern_base;
+    uint8_t* kern_dump;
+    vm_size_t kern_size;
+};
+ 
+#endif /* YaluKernel_h */
+ 
+ 
+ 
+//
+//  IOExternalTrapPrimitives.h
+//  Copyright © 2016 qwertyoruiop. All rights reserved.
+//
